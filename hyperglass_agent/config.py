e33d155d92b5311b064fefec65a34cc9f96bbd48
checktheroads <matt@stellar.tech>
2020-01-05 01:25:53 -0700

    move file reading to async

--- hyperglass_agent/config.py
+++ hyperglass_agent/config.py
@@ -1,10 +1,12 @@
 """Read YAML config file, validate, and set defaults."""
 
 # Standard Library Imports
+import asyncio
 from pathlib import Path
 
 # Third Party Imports
 import yaml
+from aiofile import AIOFile
 from pydantic import ValidationError
 
 # Project Imports
@@ -20,25 +22,43 @@ WORKING_DIR = Path(__file__).resolve().parent
 
 CONFIG_FILE = WORKING_DIR / "config.yaml"
 
-try:
-    with CONFIG_FILE.open("r") as config_file:
-        raw_config = yaml.safe_load(config_file)
-except FileNotFoundError:
-    raise ConfigError(
-        "Config file not found. A config file is required for authentication at least."
-    ) from None
-except (yaml.YAMLError, yaml.MarkedYAMLError) as yaml_error:
-    raise ConfigError(yaml_error) from None
+
+async def get_config():
+    """Read config file & load YAML to dict.
+
+    Raises:
+        ConfigError: Raised if file is not found.
+        ConfigError: Raised if there is a YAML parsing error.
+
+    Returns:
+        {dict} -- Loaded config
+    """
+    try:
+        async with AIOFile(CONFIG_FILE, "r") as file:
+            config_file = await file.read()
+            raw_config = yaml.safe_load(config_file)
+
+    except FileNotFoundError:
+        raise ConfigError("Config file not found.") from None
+
+    except (yaml.YAMLError, yaml.MarkedYAMLError) as yaml_error:
+        raise ConfigError(yaml_error) from None
+    return raw_config
+
+
+raw_config = asyncio.run(get_config())
 
 try:
     raw_config_commands = raw_config.pop("commands", None)
     user_config = General(**raw_config)
+
     if raw_config_commands is not None:
         user_commands = Commands.import_params(
             mode=user_config.mode, **raw_config_commands
         )
     else:
         user_commands = Commands.import_params(mode=user_config.mode)
+
 except ValidationError as validation_errors:
     errors = validation_errors.errors()
     for error in errors:
